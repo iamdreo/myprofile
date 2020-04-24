9f71aeb7c72a0920361f8f6a0f59c0cba32e35c7
Andrew Ole <andrew_ole@yahoo.com>
2020-04-24 10:09:09 +0100

    Updates

---
+++ precache-manifest.5a55b76c6b1dca0875bfa51afda10182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfd30c0750cd777a9aad",
+    "url": "/myprofile/static/css/main.604c9d61.chunk.css"
+  },
+  {
+    "revision": "cfd30c0750cd777a9aad",
+    "url": "/myprofile/static/js/main.cfd30c07.chunk.js"
+  },
+  {
+    "revision": "6ffbbab58a218cfc9588",
+    "url": "/myprofile/static/js/runtime~main.6ffbbab5.js"
+  },
+  {
+    "revision": "1250219a0657e466a8bb",
+    "url": "/myprofile/static/css/2.241970bc.chunk.css"
+  },
+  {
+    "revision": "1250219a0657e466a8bb",
+    "url": "/myprofile/static/js/2.1250219a.chunk.js"
+  },
+  {
+    "revision": "02b0bf574f097fb809149c549297875f",
+    "url": "/myprofile/index.html"
+  }
+];
